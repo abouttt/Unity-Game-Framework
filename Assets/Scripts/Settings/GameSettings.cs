7a9ae9eccfa3fa628fbb31ba0075d4bf778fecb4
DaeHan Jang <abouttt57@gmail.com>
2025-06-28 22:52:37 +0900

    SoundManager 추가

--- Assets/Scripts/Settings/GameSettings.cs
+++ Assets/Scripts/Settings/GameSettings.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.InputSystem;
 
 [CreateAssetMenu(fileName = "GameSettings", menuName = "Settings/Game Settings")]
@@ -7,4 +8,11 @@ public class GameSettings : ScriptableObject
     [field: Header("Input Settings")]
     [field: SerializeField]
     public InputActionAsset InputActions { get; private set; }
+
+    [field: Header("Sound Settings")]
+    [field: SerializeField]
+    public AudioMixer AudioMixer { get; private set; }
+
+    [field: SerializeField]
+    public GameObject DDDAudioPrefab { get; private set; }
 }
